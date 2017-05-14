f2d95a949ac39792cb48ad3f7bf7b847b45e45a6
Alex Hall <ansible1@earthbound.io>
2017-05-14 00:07:16 -0600

    delete file tree now in dev branch

--- devAndReference/syncFilters-HERMIOINE.bat
+++
@@ -1,3 +0,0 @@
-robocopy "C:\Users\Tia\AppData\Roaming\Filter Forge 4\System\Library" "\\DUMBLEDORE\C$\Users\Alex\AppData\Roaming\Filter Forge 4\System\Library"
-
-robocopy "\\DUMBLEDORE\C$\Users\Alex\AppData\Roaming\Filter Forge 4\System\Library" "C:\Users\Tia\AppData\Roaming\Filter Forge 4\System\Library"
--- devAndReference/syncFilters.bat
+++
@@ -1,3 +0,0 @@
-robocopy "C:\Users\Alex\AppData\Roaming\Filter Forge 4\System\Library" "\\HERMIOINE\C$\Users\Tia\AppData\Roaming\Filter Forge 4\System\Library"
-
-robocopy "\\HERMIOINE\C$\Users\Tia\AppData\Roaming\Filter Forge 4\System\Library" "C:\Users\Alex\AppData\Roaming\Filter Forge 4\System\Library"
